e94973ef6474f3c61969af6408ccedf701b099e4
ReaperRadness <MikhailAshshidiq@gmail.com>
2023-01-18 20:16:15 +0700

    initial commit

--- hello_world.py
+++ hello_world.py
@@ -4,10 +4,9 @@ import openai
 def open_file(filepath):
     with open(filepath, 'r', encoding='utf-8') as infile:
         return infile.read()
-
+        
 
 openai.api_key = open_file('openaiapikey.txt')
-
 
 def gpt3_completion(prompt, engine='text-davinci-002', temp=0.7, top_p=1.0, tokens=400, freq_pen=0.0, pres_pen=0.0, stop=['<<END>>']):
     prompt = prompt.encode(encoding='ASCII',errors='ignore').decode()
@@ -20,10 +19,9 @@ def gpt3_completion(prompt, engine='text-davinci-002', temp=0.7, top_p=1.0, toke
         frequency_penalty=freq_pen,
         presence_penalty=pres_pen,
         stop=stop)
-    text = response['choices'][0]['text'].strip()
+    text = response ['choices'][0]['text'].strip()
     return text
-
-
+        
 if __name__ == '__main__':
     prompt = 'Write a list of famous American actors:'
     response = gpt3_completion(prompt)
